303633338f48ca0f1728162319d2cc11af7c71df
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-10 13:15:19 -0700

    passing props to styled components

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -2,16 +2,16 @@ import { useDispatch } from "react-redux"
 import { markTodoAsCompleted ,deleteTodo } from "./thunks";
 import styled from "styled-components";
 const CardContainer=styled.div`
+${props=>props.important?'background-color:yellow;':'background-color:white;'}
 border-radius:10px;
 box-shadow:0 2px 5px rgba(0,0,0.1,0.5);
 padding:15px;
- background-color:white;
- color:black`;
+color:black`;
 
 export default function TodoListItem({todo}){
     const dipsatch=useDispatch();
     return(
-        <CardContainer>
+        <CardContainer importan={false}>
         <h3>{todo.text}</h3>
         {todo.isCompleted && <p>Completed</p>}
         {todo.isCompleted?
