29fac7aaa7be2328cd91d1af5797191fa3c73fb3
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-10 13:10:09 -0700

    created a basic stylong using styled from styled components

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -1,16 +1,23 @@
 import { useDispatch } from "react-redux"
-import { markTodoAsCompleted } from "./thunks";
-import { deleteTodo } from "./thunks";
+import { markTodoAsCompleted ,deleteTodo } from "./thunks";
+import styled from "styled-components";
+const CardContainer=styled.div`
+border-radius:10px;
+box-shadow:0 2px 5px rgba(0,0,0.1,0.5);
+padding:15px;
+ background-color:white;
+ color:black`;
+
 export default function TodoListItem({todo}){
     const dipsatch=useDispatch();
     return(
-        <>
+        <CardContainer>
         <h3>{todo.text}</h3>
         {todo.isCompleted && <p>Completed</p>}
         {todo.isCompleted?
         <button onClick={()=>dipsatch(deleteTodo(todo.id))}>Delete Item</button>
     :<button onClick={()=>dipsatch(markTodoAsCompleted(todo.id))}>Mark as complete </button>}
-        </>
+        </CardContainer>
     )
 
 }
